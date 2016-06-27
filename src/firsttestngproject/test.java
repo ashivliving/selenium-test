56cace2ebbeeb316492f311210733a9421b0e5b2
Ashiv Gupta <ashivliving@bitbucket.org>
2016-06-27 13:23:10 +0530

    different tests

--- src/firsttestngproject/test.java
+++ src/firsttestngproject/test.java
@@ -2,7 +2,6 @@ package firsttestngproject;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
