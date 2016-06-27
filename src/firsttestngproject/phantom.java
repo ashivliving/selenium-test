2538007746f0a891ff5335bcf53adc2044c4e32c
Ashiv Gupta <ashivliving@bitbucket.org>
2016-06-27 13:28:03 +0530

    test improved

--- src/firsttestngproject/phantom.java
+++ src/firsttestngproject/phantom.java
@@ -61,38 +61,6 @@ public class phantom {
 			    
 		}
 		
-		@Test(priority = 3)
-		public void checkmenubar2() throws IOException{
-			driver.manage().window().maximize();	
-			
-			String[] keywords = {"buy 1 get 1","mobile phone","snapdeal coupons",
-					             "computers coupons","Amazon india offers","fashion offer",
-					             "flipkart offers","freecharge offers","mobile recharge offers",
-					             "free samples online","shopclues offers"};
-			int error = 0;
-			for(int i=1;i<=11;i++)
-			{ String str="//*[@id='menubar']/div/div[3]/div[2]/div/a["+ i + "]";
-			  driver.findElement(By.xpath(str)).click();
-			  String str1 = driver.getTitle();
-			  String str2 = keywords[i-1];
-			  if(!(str1.toLowerCase().contains(str2.toLowerCase())))
-			  {		error++;
-				  	File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-				    try{
-				    FileUtils.copyFile(scrFile, new File("/home/ashivliving/workspace/FirstTestNGProject/src/firsttestngproject/image/image" + error +".jpg"),true);
-				    System.out.println("Error occur at "+ str2 + " page!");
-				    }  catch(Exception e){
-				    	System.out.println("Can't capture screenshot!");
-				    }
-			  }
-			}
-			if(error==0)
-				System.out.println("Menubar 2 working correctly");
-			else
-				System.out.println("There are "+ error + " errors!");
-			
-		}
-		
 		@AfterSuite
 		 public void tearDown(){
 			 driver.quit();
