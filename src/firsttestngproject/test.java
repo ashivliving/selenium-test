0e7fc5442316b5427c23e1ee167fc811eefd3e33
Ashiv Gupta <ashivliving@bitbucket.org>
2016-06-27 17:37:47 +0530

    Footer added

--- src/firsttestngproject/test.java
+++ src/firsttestngproject/test.java
@@ -17,13 +17,13 @@ import org.testng.annotations.Test;
 public class test {	
 	public WebDriver driver;
 	 private String baseUrl;
-	 
+	 public String phantomloc = "/usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs";
 		@BeforeSuite
 		public void setUp(){
 			DesiredCapabilities caps = new DesiredCapabilities();
 			 caps.setJavascriptEnabled(true);
 			 caps.setCapability("takesScreenshot", true);
-			 caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"/usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs");
+			 caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,phantomloc);
 			 driver = new PhantomJSDriver(caps);
 			 baseUrl = "http://www.mytokri.com";
 			 driver.get(baseUrl);
@@ -47,7 +47,7 @@ public class test {
 			  {		error++;
 				  	File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 				    try{
-				    FileUtils.copyFile(scrFile, new File("/home/ashivliving/workspace/FirstTestNGProject/src/firsttestngproject/image/image" + error +".jpg"),true);
+				    FileUtils.copyFile(scrFile, new File("/home/ashivliving/workspace/FirstTestNGProject/src/firsttestngproject/image/menu2" + error +".jpg"),true);
 				    System.out.println("Error occur at "+ str2 + " page!");
 				    }  catch(Exception e){
 				    	System.out.println("Can't capture screenshot!");
@@ -59,6 +59,99 @@ public class test {
 			else
 				System.out.println("There are "+ error + " errors!");
 			
+		}
+		
+		@Test(priority = 2)
+		public void checkfooter()
+		{
+			driver.manage().window().maximize();
+			String[] keyword_about = {"about us","privacy policy","sitemap",
+					"advertise with us","blog",
+					"careers with us","contact us"};
+			String[] keyword_category = {"cameras coupons","babycare","fashion offers",
+										 "food & drink","books & stationery Coupons",
+										 "mobile recharge offers","home & kitchen","mobile phone"
+										 ,"tablets coupons","computers coupons","find all coupon"};
+			String[] keyword_store = {"amazon india","flipkart offers","snapdeal coupons",
+									  "myntra offers","jabong latest offers","zivame coupons",
+									  "foodpanda coupons","dominos coupons","firstcry coupons",
+									  "ebay india coupons","all coupon stores"};
+			String[] keyword_occasion = {"valentine day offers","holi offers","christmas offers",
+										 "diwali offers","festivals deals"};
+			//========> About us <===================== 
+			
+			int error = 0;
+			for(int i=2;i<=8;i++)
+			{	if(i==6)
+				 continue;
+				String str = "//*[@id='footertop']/div/div/section[1]/ul/li[" + i +"]/a";
+				driver.findElement(By.xpath(str)).click();
+				String str1 = driver.getTitle();
+				String str2 = keyword_about[i-2];
+				if(!(str1.toLowerCase().contains(str2.toLowerCase())))
+				  {		error++;
+					  	System.out.println("Error occur at ABOUT section : "+ str2 + " page!");  
+				  }
+			}
+				if(error==0)
+					System.out.println("No error in Footer ABOUT");
+				else
+					System.out.println("There are "+ error + " errors in footer ABOUT!");	
+
+			//============> Category <=============
+				error = 0;
+				for(int i=1;i<=11;i++)
+				{	
+					String str = "//*[@id='footertop']/div/div/section[2]/ul/li["+i+"]/a";
+					driver.findElement(By.xpath(str)).click();
+					String str1 = driver.getTitle();
+					String str2 = keyword_category[i-1];
+					if(!(str1.toLowerCase().contains(str2.toLowerCase())))
+					  {		error++;
+						  	System.out.println("Error occur at CATEGORY section : "+ str2 + " page!");  
+					  }
+				}
+					if(error==0)
+						System.out.println("No error in Footer CATEGORY");
+					else
+						System.out.println("There are "+ error + " errors in footer CATEGORY!");
+					
+			//==========> Stores <==========
+					error = 0;
+					for(int i=1;i<=11;i++)
+					{	
+						String str = "//*[@id='footertop']/div/div/section[3]/ul/li["+i+"]/a";
+						driver.findElement(By.xpath(str)).click();
+						String str1 = driver.getTitle();
+						String str2 = keyword_store[i-1];
+						if(!(str1.toLowerCase().contains(str2.toLowerCase())))
+						  {		error++;
+							  	System.out.println("Error occur at STORES section : "+ str2 + " page!");  
+						  }
+						
+					}
+						if(error==0)
+							System.out.println("No error in Footer STORES");
+						else
+							System.out.println("There are "+ error + " errors in footer STORES!");
+			//==========> Occasion <=============			
+						error = 0;
+						for(int i=1;i<=5;i++)
+						{	
+							String str = "//*[@id='footertop']/div/div/section[4]/ul/li["+i+"]/a";
+							driver.findElement(By.xpath(str)).click();
+							String str1 = driver.getTitle();
+							String str2 = keyword_occasion[i-1];
+							if(!(str1.toLowerCase().contains(str2.toLowerCase())))
+							  {		error++;
+								  	System.out.println("Error occur at Occasion section : "+ str2 + " page!");  
+							  }
+							
+						}
+							if(error==0)
+								System.out.println("No error in Footer Occasion");
+							else
+								System.out.println("There are "+ error + " errors in footer Occasion!");
 		}
 		
 		
