88e0ec34994f7a6cfb51b5b52d9922f35cbce1ca
Ashiv Gupta <ashivliving@bitbucket.org>
2016-07-08 12:38:33 +0530

    Test

--- src/firsttestngproject/temp.java
+++ src/firsttestngproject/temp.java
@@ -32,49 +32,16 @@ public class temp {
 			 caps.setCapability("takesScreenshot", true);
 			 caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,phantomloc);
 			 driver = new PhantomJSDriver(caps);
-			 baseUrl = "http://www.mytokri.com";
+			 baseUrl = "http://www.couponhind.com/";
 			 driver.get(baseUrl);
 			 driver.manage().window().maximize();
 		}
 		
-		@Test(priority = 2)
-        public void sidebar_popular() throws FileNotFoundException, UnsupportedEncodingException
-        {
-                try(FileWriter fw = new FileWriter("result.txt", true);
-                            BufferedWriter bw = new BufferedWriter(fw);
-                            PrintWriter out = new PrintWriter(bw))
-                        {
-
-                int error = 0;
-                String sidexpath,str1,str2;
-                for(int i=1;i<=1;i++)
-                {
-                        try{
-                                sidexpath = "//*[@id='sidebar']/div[8]/table/tbody/tr["+i+"]/td[1]/a";
-                                str1 = driver.findElement(By.xpath(sidexpath)).getText();
-                        }
-                        catch(NoSuchElementException e){
-                                sidexpath = "//*[@id='sidebar']/div[9]/table/tbody/tr["+i+"]/td[1]/a";
-                                str1 = driver.findElement(By.xpath(sidexpath)).getText();
-                        }
-                        if(str1.length()<=0)
-                        {
-                                error++;
-                                out.println("Error in Sidebar Popular Deals at "+ str1 +" Link.");
-                        }
-
-
-                        driver.navigate().to(baseUrl);
-                }
-                if(error==0)
-                        out.println("No error in Sidebar Popular Deals");
-                else
-                        out.println("There are "+ error + " errors in Sidebar Popular Deals!");
-                        }catch(IOException e){
-
-                        }
-        }
-
+		@Test(priority = 1)
+		public void testing(){
+			
+			System.out.println(driver.getTitle());
+		}
 		
 		@AfterSuite
 		 public void tearDown(){
